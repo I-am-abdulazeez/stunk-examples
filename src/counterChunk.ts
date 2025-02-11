01b94538435bf9268e9aa4f32b8f58be6f04fb3d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-11 17:25:22 +0100

    add counter setup functionality with chunk integration

---
+++ src/counterChunk.ts
@@ -0,0 +1,29 @@
+import { chunk } from "stunk";
+
+type counterArgs = {
+  counterBtn: HTMLButtonElement,
+  count: HTMLParagraphElement;
+  doubledCount: HTMLParagraphElement
+}
+
+export function setupCounter({ counterBtn, count, doubledCount }: counterArgs) {
+  const counter = chunk(0);
+  const doubledCounter = counter.derive((value) => value * 2);
+  const isDisabled = counter.derive((value) => value >= 20)
+
+  counter.subscribe((value) => {
+    count.textContent = `Count: ${value}`
+  });
+  doubledCounter.subscribe((value) => {
+    doubledCount.textContent = `Double: ${value}`
+  });
+
+  isDisabled.subscribe((disabled) => {
+    counterBtn.disabled = disabled;
+  });
+
+  counterBtn.addEventListener('click', () => {
+    counter.update((value) => value + 1);
+  });
+
+}
