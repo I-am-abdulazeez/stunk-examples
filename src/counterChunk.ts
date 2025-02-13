efe4f2c86464a9e8ca37ee95f3f4d633f9a09ad2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-13 13:59:15 +0100

    feat: integrate chunk and computed for enhanced data handling in setupCounter

--- src/counterChunk.ts
+++ src/counterChunk.ts
@@ -1,4 +1,4 @@
-import { chunk } from "stunk";
+import { chunk, computed } from "stunk";
 
 type counterArgs = {
   counterBtn: HTMLButtonElement,
@@ -10,6 +10,8 @@ export function setupCounter({ counterBtn, count, doubledCount }: counterArgs) {
   const counter = chunk(0);
   const doubledCounter = counter.derive((value) => value * 2);
   const isDisabled = counter.derive((value) => value >= 20)
+
+
 
   counter.subscribe((value) => {
     count.textContent = `Count: ${value}`
@@ -25,5 +27,22 @@ export function setupCounter({ counterBtn, count, doubledCount }: counterArgs) {
   counterBtn.addEventListener('click', () => {
     counter.update((value) => value + 1);
   });
+
+  const firstNameChunk = chunk('John');
+  const lastNameChunk = chunk('Doe');
+  const ageChunk = chunk(30);
+
+  const fullInfoChunk = computed(
+    [firstNameChunk, lastNameChunk, ageChunk],
+    (firstName, lastName, age) => ({
+      fullName: `${firstName} ${lastName}`,
+      isAdult: age >= 18,
+    })
+  );
+
+  firstNameChunk.set("Jane");
+  ageChunk.set(10)
+
+  console.log(fullInfoChunk.get()); // ✅ Prints: { fullName: "Jane Doe", isAdult: true }
 
 }
