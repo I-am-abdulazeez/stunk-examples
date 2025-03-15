4297aa626dcb9941394f4baad4df2b2ebf6dac8d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-16 00:46:12 +0100

    chore: corrections on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,23 @@ export default function Index() {
   return (
     <>
       <Brands />
-      <h1 className="text-4xl font-semibold">Vite + React + Stunk</h1>
-      <h3 className="text-2xl my-5">
-        This examples demonstrates how to use Stunk with React.
-      </h3>
-      <p className="text-xl">
-        Click on the routes below to see Stunk in action.
-      </p>
-      <div className="flex items-center justify-between mt-10">
-        <Link to={"/counter"}>
-          <button className="btn btn-xs md:btn-lg">Counter</button>
-        </Link>
+      <div className="px-10">
+        <h1 className="text-3xl md:text-4xl font-semibold">
+          Vite + React + Stunk
+        </h1>
+        <div>
+          <h3 className="text-xl md:text-2xl my-5">
+            These examples demonstrates how to use Stunk with React.
+          </h3>
+        </div>
+        <div className="flex items-center gap-4 mt-10">
+          <Link to={"/counter"}>
+            <button className="btn btn-lg">Counter</button>
+          </Link>
+          <Link to={"/todo"}>
+            <button className="btn btn-lg">Todo</button>
+          </Link>
+        </div>
       </div>
     </>
   );
