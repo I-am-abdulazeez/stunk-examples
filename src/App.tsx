ea2d6881f1979b71e6fea4ac44555624076dced5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-16 00:46:28 +0100

    chore: Todo route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Index from "./pages";
 import Counter from "./pages/counter";
+import Todo from "./pages/todo";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/counter" element={<Counter />} />
+      <Route path="/todo" element={<Todo />} />
     </Routes>
   );
 }
