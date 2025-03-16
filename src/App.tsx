d89bb92dcf609dde08a2593f09a59228379ed17a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-16 16:56:23 +0100

    feat: Auth Manager page registration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Index from "./pages";
 import Counter from "./pages/counter";
 import Todo from "./pages/todo";
 import ThemeSwitcher from "./pages/theme-switcher";
+import AuthManager from "./pages/auth-manager";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/counter" element={<Counter />} />
       <Route path="/todo" element={<Todo />} />
       <Route path="/theme-switch" element={<ThemeSwitcher />} />
+      <Route path="/auth-manager" element={<AuthManager />} />
     </Routes>
   );
 }
