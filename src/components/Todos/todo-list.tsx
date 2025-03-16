5a802883498b6bc266256c6cec38bbf95edaeb1e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-16 10:39:56 +0100

    chore: refactor component

--- src/components/Todos/todo-list.tsx
+++ src/components/Todos/todo-list.tsx
@@ -45,54 +45,52 @@ export default function TodoList() {
           <p>Add some tasks or change your filter</p>
         </div>
       ) : (
-        <div>
-          <ul className="divide-y divide-gray-200">
-            {filteredTodos.map((todo) => {
-              const category = categories.find((c) => c.id === todo.categoryId);
+        <ul className="divide-y divide-gray-200">
+          {filteredTodos.map((todo) => {
+            const category = categories.find((c) => c.id === todo.categoryId);
 
-              return (
-                <li
-                  key={todo.id}
-                  className="flex items-center justify-between p-4 hover:bg-gray-50"
-                >
-                  <div className="flex items-start flex-1 gap-3">
-                    <input
-                      type="checkbox"
-                      checked={todo.completed}
-                      onChange={() => toggleTodo(todo.id)}
-                      className="checkbox checkbox-primary mt-1"
-                    />
-                    <div>
-                      <p
-                        className={`${
-                          todo.completed
-                            ? "line-through text-gray-500"
-                            : "text-gray-800"
-                        } font-medium text-xl text-left`}
-                      >
-                        {todo.text}
-                      </p>
-                      <div className="flex gap-2 items-center">
-                        <span className="bg-amber-200 px-1 text-gray-800 rounded-md">
-                          {category?.name}
-                        </span>
-                        <span className="text-sm text-gray-500">
-                          {new Date(todo.createdAt).toLocaleDateString()}
-                        </span>
-                      </div>
+            return (
+              <li
+                key={todo.id}
+                className="flex items-center justify-between p-4 hover:bg-gray-50"
+              >
+                <div className="flex items-start flex-1 gap-3">
+                  <input
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => toggleTodo(todo.id)}
+                    className="checkbox checkbox-primary mt-1"
+                  />
+                  <div>
+                    <p
+                      className={`${
+                        todo.completed
+                          ? "line-through text-gray-500"
+                          : "text-gray-800"
+                      } font-medium text-xl text-left`}
+                    >
+                      {todo.text}
+                    </p>
+                    <div className="flex gap-2 items-center">
+                      <span className="bg-cyan-300 text-sm font-medium px-1 text-gray-800 rounded-md">
+                        {category?.name}
+                      </span>
+                      <span className="text-sm text-gray-500">
+                        {new Date(todo.createdAt).toLocaleDateString()}
+                      </span>
                     </div>
                   </div>
-                  <button
-                    className="btn btn-circle btn-error btn-sm"
-                    onClick={() => deleteTodo(todo.id)}
-                  >
-                    <DeleteIcon />
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                </div>
+                <button
+                  className="btn btn-circle btn-error btn-sm"
+                  onClick={() => deleteTodo(todo.id)}
+                >
+                  <DeleteIcon />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       )}
     </div>
   );
