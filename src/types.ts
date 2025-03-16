c8be2b50cd5efeb520c13687c4f5551341bbc299
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-16 01:30:07 +0100

    types(components): added auth types

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,11 @@ export type Category = {
   id: string;
   name: string;
 };
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export type isAuth = { user: User | null; isAuthenticated: boolean }
