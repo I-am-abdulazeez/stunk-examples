c3cbb0940ef6e5e079a23f55adbb00fd9af8f42a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-17 13:56:35 +0100

    chore: btn normal

--- src/pages/auth-manager.tsx
+++ src/pages/auth-manager.tsx
@@ -13,17 +13,14 @@ export default function AuthManager() {
       <Heading text="Auth Manager" />
       <div className="flex flex-col items-center gap-4 p-6 border-2 border-gray-700 rounded-lg">
         {auth.loading ? (
-          <button className="btn btn-lg">
+          <button className="btn">
             <span className="loading loading-spinner"></span>
             loading
           </button>
         ) : auth.isAuthenticated ? (
           <>
             <p className="text-lg">Welcome, {auth.user?.name}!</p>
-            <button
-              onClick={logout}
-              className="btn btn-error btn-lg shadow-none"
-            >
+            <button onClick={logout} className="btn btn-error shadow-none">
               Logout
             </button>
           </>
@@ -36,7 +33,7 @@ export default function AuthManager() {
                 email: "olamide@example.com",
               })
             }
-            className="btn btn-success shadow-none btn-lg"
+            className="btn btn-success shadow-none"
           >
             Login
           </button>
