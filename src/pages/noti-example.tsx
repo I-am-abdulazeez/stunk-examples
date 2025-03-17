759ff01b082a709463988465a93207b82ae9d0fe
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-17 14:10:51 +0100

    feat: Notification Example

---
+++ src/pages/noti-example.tsx
@@ -0,0 +1,16 @@
+import Heading from "@/components/shared/heading";
+import NotiStyle from "@/components/notification/noti-style";
+import GoBack from "@/components/shared/go-back";
+
+export default function NotifyExample() {
+  return (
+    <>
+      <Heading text="Notification" />
+      <NotiStyle header="Default" variant="default" />
+      <NotiStyle header="Soft" variant="soft" />
+      <NotiStyle header="Outline" variant="outline" />
+      <NotiStyle header="Dashed" variant="dashed" />
+      <GoBack />
+    </>
+  );
+}
