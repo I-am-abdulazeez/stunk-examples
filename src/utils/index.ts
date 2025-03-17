72c66c6c5bb2ad0a672898efa1a2d3d57c294959
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-17 13:53:16 +0100

    feat: getAlertClass util to manage notification class

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
 export const numberWithComma = (number: string) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const getAlertClass = (
+  type: "success" | "error" | "info" | "warning",
+  variant: "default" | "soft" | "outline" | "dashed"
+) => {
+  const baseClass = `alert alert-${type}`;
+
+  const variants = {
+    default: baseClass,
+    soft: `${baseClass} alert-soft`,
+    outline: `${baseClass} alert-outline`,
+    dashed: `${baseClass} alert-border`,
+  };
+
+  return variants[variant];
+};
