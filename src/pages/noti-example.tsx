316959f3cf5cdf0508448f2fb190edf72807c409
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-18 09:55:59 +0100

    chore: remove margin styles since notify button

--- src/pages/noti-example.tsx
+++ src/pages/noti-example.tsx
@@ -4,13 +4,13 @@ import GoBack from "@/components/shared/go-back";
 
 export default function NotifyExample() {
   return (
-    <div className="my-4">
+    <>
       <Heading text="Notification" />
       <NotiStyle header="Default" variant="default" />
       <NotiStyle header="Soft" variant="soft" />
       <NotiStyle header="Outline" variant="outline" />
-      <NotiStyle header="Dashed" variant="dashed" />
+      <NotiStyle header="Dashed" variant="dash" />
       <GoBack />
-    </div>
+    </>
   );
 }
