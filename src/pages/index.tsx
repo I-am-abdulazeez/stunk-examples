a27a1982a3d80fa0aaf87afb6c648eaedc1efc37
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-03-18 07:20:35 +0100

    styles: wrap up component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,15 @@ export default function Index() {
     <>
       <Brands />
       <div className="px-10">
-        <h1 className="text-2xl md:text-4xl font-semibold">
-          Vite + React + Stunk
-        </h1>
+        <h1 className="text-2xl md:text-4xl font-bold">Vite + React + Stunk</h1>
         <div>
           <h3 className="text-xl md:text-2xl my-5">
             These examples demonstrates how to use Stunk with React.
           </h3>
         </div>
         <div className="flex items-center gap-4 mt-10 flex-wrap max-w-[500px] mx-auto justify-center">
-          <AppLink to="/" text="Counter" />
-          <AppLink to="/todo" text="Todo" />
+          <AppLink to="/counter" text="Counter" />
+          <AppLink to="/todo" text="Task Manager" />
           <AppLink to="/theme-switch" text="Theme Switch" />
           <AppLink to="/auth-manager" text="Auth Manager" />
           <AppLink to="/cart" text="Shopping Cart" />
@@ -24,7 +22,7 @@ export default function Index() {
           <AppLink to="/form-manager" text="Form Manager" />
           <AppLink to="/notify" text="Notification" />
           <AppLink to="/multi-step" text="Multi Step Wizard" />
-          <AppLink to="/kanaban" text="Kanban" />
+          <AppLink to="/kanaban" text="Kanban Board" />
         </div>
       </div>
     </>
