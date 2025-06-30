b45423623ebb3ddfb754f47160ecf01e3c60a627
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-06-30 13:56:03 +0100

    feat: add link to Modal Page in the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ export default function Index() {
           <AppLink to="/notify" text="Notification" />
           <AppLink to="/multi-step" text="Multi Step Wizard" />
           <AppLink to="/kanban" text="Kanban Board (lg screens only)" />
+          <AppLink to="/modal" text="Modal Page " />
         </div>
       </div>
     </>
