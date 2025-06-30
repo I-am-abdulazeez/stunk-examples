1270fab667fb342b77a324a36a033bfc77e5d636
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-06-30 13:55:56 +0100

    feat: add mock data for courses

---
+++ src/data/index.ts
@@ -0,0 +1,15 @@
+// Mock data
+export const courses = [
+  {
+    courseTitle: "London",
+    courseContent: "London Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore deleniti assumenda quas minus, fugiat veniam necessitatibus laborum qui similique dolor!"
+  },
+  {
+    courseTitle: "Geography",
+    courseContent: "Geography Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore deleniti assumenda quas minus, fugiat veniam necessitatibus laborum qui similique dolor!"
+  },
+  {
+    courseTitle: "Mathematics",
+    courseContent: "Mathematics Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore deleniti assumenda quas minus, fugiat veniam necessitatibus laborum qui similique dolor!"
+  }
+];
