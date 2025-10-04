6b1e042a970f0e69cfbc27cfbbb20cd0a97b32ec
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-04 08:09:58 +0100

    chore: update component fetching

--- src/pages/users-list.tsx
+++ src/pages/users-list.tsx
@@ -12,15 +12,19 @@ export default function UserList() {
     <div className="p-4">
       <Heading text="Users List" />
       {loading && <p className="text-blue-500">Loading...</p>}
-      {error && <p className="text-red-500">Error: {error.message}</p>}
+      {!loading && error && (
+        <p className="text-red-500">Error: {error.message}</p>
+      )}
 
-      <ul className="divide-y divide-gray-600 mt-5">
-        {data?.map((user) => (
-          <li key={user.id} className="py-2">
-            {user.name}
-          </li>
-        ))}
-      </ul>
+      {!loading && data && data.length > 0 && (
+        <ul className="divide-y divide-gray-600 mt-5">
+          {data?.map((user) => (
+            <li key={user.id} className="py-2">
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      )}
 
       <button onClick={() => reload()} className="btn btn-primary mt-4">
         Reload
