9b50d4840c4cb21a105bdd67f9b703b5c18b50dc
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-05 14:36:58 +0100

    chore: new header design

--- src/components/shared/heading.tsx
+++ src/components/shared/heading.tsx
@@ -1,8 +1,12 @@
 export default function Heading({ text }: { text: string }) {
   return (
-    <div className="text-center mb-3">
-      <h1 className="text-4xl font-bold text-indigo-300 mb-2">{text}</h1>
-      <div className="h-1 w-24 bg-indigo-500 mx-auto rounded"></div>
+    <div className="text-center my-8">
+      <h1 className="text-4xl md:text-5xl font-black mb-3">{text}</h1>
+      <div className="flex items-center justify-center gap-2">
+        <div className="h-1 w-8 bg-[#2af4c2]/30 rounded-full"></div>
+        <div className="h-1 w-16 bg-[#2af4c2] rounded-full"></div>
+        <div className="h-1 w-8 bg-[#2af4c2]/30 rounded-full"></div>
+      </div>
     </div>
   );
 }
