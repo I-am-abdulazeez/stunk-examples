238d599a993d43eab01e0c4f776cf4daebfc709b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-05 14:43:17 +0100

    chore: Goback design

--- src/components/shared/go-back.tsx
+++ src/components/shared/go-back.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { Home, ArrowLeft } from "lucide-react";
 
 export default function GoBack() {
   return (
-    <div className="mt-4">
-      <Link className="link link-hover" to={"/"}>
-        Go home
+    <div className="flex justify-center mt-8">
+      <Link to="/" className="btn btn-ghost gap-2 group">
+        <ArrowLeft className="w-4 h-4 group-hover:-translate-x-1 transition-transform" />
+        <Home className="w-4 h-4" />
+        <span>Back to Home</span>
       </Link>
     </div>
   );
