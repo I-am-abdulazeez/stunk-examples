701a127cda5f5c02ef29cfd13b6b9939b5fafb91
chibx <111882496+chibx@users.noreply.github.com>
2025-10-07 12:05:35 +0100

    Renamed Infinity to InfinityIcon
    To avoid warning from linters about trying to overshadow global variable
    Infinity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import {
   Layers,
   Layout,
   Database,
-  Infinity,
+  Infinity as InfinityIcon,
 } from "lucide-react";
 
 export default function Index() {
@@ -28,7 +28,7 @@ export default function Index() {
     { to: "/kanban", text: "Kanban Board", icon: Layout },
     { to: "/modal", text: "Modals", icon: Box },
     { to: "/users-paginated", text: "Paginated Users", icon: Database },
-    { to: "/users-infinite", text: "Infinite Scroll", icon: Infinity },
+    { to: "/users-infinite", text: "Infinite Scroll", icon: InfinityIcon },
   ];
 
   return (
